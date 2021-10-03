a8d29fde05e72fe9f798557385653dd181fc7f93
IndraP24 <indrashis985@gmail.com>
2021-10-03 09:40:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement, ReactNode } from 'react';
 import './App.css';
+
+// Conventional props
+function Heading({ title }: { title?: string; }) {
+  return (
+    <h1>{title}</h1>
+  );
+}
+
+function HeadingWithContent({ children }: { children?: ReactNode; }): ReactElement {
+  return (
+    <h1>{children}</h1>
+  );
+}
+
+// deafultProps
+const defaultContainerProps = {
+  heading: <strong>My Heading</strong>
+};
+type ContainerProps = { children: ReactNode; } & typeof defaultContainerProps;
+function Container({ heading, children }: ContainerProps): ReactElement {
+  return (
+    <div>
+      <h1>
+        {heading}
+      </h1>
+      {children}
+    </div>
+  );
+}
+
+Container.defaultProps = defaultContainerProps;
+
+// Functional props
+function TextWithNumber({
+  header,
+  children
+}: {
+  header?: (num: number) => ReactNode
+  children: (num: number) => ReactNode
+}) {
+  const [state, stateSet] = React.useState<number>(1);
+
+  return (
+    <div>
+      {header && <h2>{header?.(state)}</h2>}
+      <div>
+        {children(state)}
+      </div>
+      <div>
+        <button onClick={() => stateSet(state + 1)}>Add</button>
+      </div>
+    </div>
+  );
+}
+
+// List
+function List<ListItem>({
+  items,
+  render,
+}: {
+  items: ListItem[],
+  render: (item: ListItem) => ReactNode
+}) {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>
+          {render(item)}
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading title="Hello there"></Heading>
+      <HeadingWithContent><strong>Hi!</strong></HeadingWithContent>
+      <Container>
+        Foo
+      </Container>
+      <TextWithNumber header={(num: number) => <span>Header {num}</span>}>
+        {(num: number) => <div>Today's number is {num}</div>}
+      </TextWithNumber>
+      <List items={["Jack", "Sadie", "oso"]} render={(item: string) => <div>{item.toLowerCase()}</div>}></List>
     </div>
   );
 }
